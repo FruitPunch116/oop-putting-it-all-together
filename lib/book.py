b5a470a92d754d815032adfcc49402a02bf7fcf5
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-08-23 14:41:53 -0400

    create book object and params

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = page_count
+
+    def turn_page(self):
+        # self.page_count += 1
+        print("Flipping the page...wow, you read fast!")
     
-        
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, new_count):
+        if type(new_count) == int:
+            self._page_count = new_count
+        else:
+            print('page_count must be an integer')
