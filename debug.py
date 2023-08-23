7b1e5245e7e23547cf754d9e691df2b86e6e64de
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-08-23 15:11:58 -0400

    create two object for each class

--- debug.py
+++ debug.py
@@ -1,7 +1,13 @@
 import ipdb
 from lib.book import *
+from lib.shoe import *
 
 cradle = Book("Cradle", 200)
+games_of_thrones2 = Book("A Clash Of Kings", 761)
+
+BB_550 = Shoe("New Balance", 11)
+core_650 = Shoe("New Balance", 11)
+
 
 if __name__ == '__main__':
     ipdb.set_trace()
